98f26ed64098be0be40a5d0050ee432f954a1ae7
DhruvMehta323 <dhrruvmehta2204@gmail.com>
2024-02-01 03:23:23 +0530

    Alternate

--- src/pages/home_page/ParallaxComponent.jsx
+++ src/pages/home_page/ParallaxComponent.jsx
@@ -1,45 +1,34 @@
-// ParallaxComponent.jsx
-import React, { useEffect, useState } from "react";
+import React from "react";
 import first from "../../assets/recommend/first.jpg";
 import second from "../../assets/recommend/second.jpg";
 import third from "../../assets/recommend/third.jpg";
 import fourth from "../../assets/recommend/fourth.jpg";
 import fifth from "../../assets/recommend/fifth.jpg";
-import "./ParallaxComponent.css";
 
-const ParallaxComponent = () => {
-  const [scrollY, setScrollY] = useState(0);
+const ImageWithLabel = ({ imageSrc, label, isEven }) => (
+  <div style={{ display: 'flex', alignItems: 'center', marginLeft: '20px',marginRight:"20px",paddingLeft:"20px", marginBottom: '20px' ,border:"solid", margin:"10px" }}>
+    <div style={{ flex: 1, order: isEven ? 1 : 2 }}>
+      <img src={imageSrc} alt={label} style={{  width: "75%", height: "75%", paddingTop:"50px", paddingBottom:"50px" }} />
+    </div>
+    <div style={{ flex: 1, order: isEven ? 2 : 1,  }}>
+      <div style={{ display: "flex", justifyContent: "center", fontSize: "2em" }}>{label}</div>
+    </div>
+  </div>
+);
 
-  const handleScroll = () => {
-    setScrollY(window.scrollY);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
+const Image = () => {
   return (
-    <div className="">
-      <div className="" style={{ transform: `translateY(${scrollY * 0.1}px)` }}>
-        <img src={first} alt="Recommendation 1" />
-      </div>
-      <div className="" style={{ transform: `translateY(${scrollY * 0.2}px)` }}>
-        <img src={second} alt="Recommendation 2" />
-      </div>
-      <div className="" style={{ transform: `translateY(${scrollY * 0.3}px)` }}>
-        <img src={third} alt="Recommendation 3" />
-      </div>
-      <div className="" style={{ transform: `translateY(${scrollY * 0.4}px)` }}>
-        <img src={fourth} alt="Recommendation 4" />
-      </div>
-      <div className="" style={{ transform: `translateY(${scrollY * 0.5}px)` }}>
-        <img src={fifth} alt="Recommendation 5" />
-      </div>
+    <div>
+      <ImageWithLabel imageSrc={first} label="Image 1" isEven={true} />
+      <ImageWithLabel imageSrc={second} label="Image 2" isEven={false}/>
+      <ImageWithLabel imageSrc={third} label="Image 3" isEven={true} />
+      <ImageWithLabel imageSrc={fourth} label="Image 4" isEven={false}/>
+      <ImageWithLabel imageSrc={fifth} label="Image 5" isEven={true} />
+      {/* <ImageWithLabel imageSrc={second} label="Image 2" isEven={false}/> */}
+      
+      {/* Add more ImageWithLabel components as needed */}
     </div>
   );
 };
 
-export default ParallaxComponent;
+export default Image;
